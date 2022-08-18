e8375c78895d0341c88def5b578fe06246847e49
nagasakikoutaro <kmny03@gmail.com>
2022-08-19 08:21:40 +0900

    投稿を保存する機能追加

--- app/Http/Controllers/BodyTemperatureController.php
+++ app/Http/Controllers/BodyTemperatureController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\BodyTemperature;
 
 class BodyTemperatureController extends Controller
 {
     public function create(){
+
+
         return view('BodyTemperature.create');
     }
 
     public function post(){
+
+      
+
         return redirect('/index');
     }
 
