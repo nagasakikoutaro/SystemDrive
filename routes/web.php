74bc5bbd6f409339f1136200e0f461678e0a7a30
nagasakikoutaro <kmny03@gmail.com>
2022-08-18 14:19:11 +0900

    体温入力・一覧ページのルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/create','App\Http\Controllers\BodyTemperatureController@create');
+Route::post('/post','App\Http\Controllers\BodyTemperatureController@post');
+Route::get('/index','App\Http\Controllers\BodyTemperatureController@index');
