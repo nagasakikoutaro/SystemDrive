52c91ad6750f8d9f18a632293b934867af165dcd
nagasakikoutaro <kmny03@gmail.com>
2022-08-19 08:29:35 +0900

    first commit

--- resources/views/BodyTemperature/create.blade.php
+++ resources/views/BodyTemperature/create.blade.php
@@ -4,15 +4,21 @@
 
 @section('content')
 
-<div class = "mainContent">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
     <h2>体温入力ページ</h2>
         <form action="/post" method="POST">
         @csrf
         <label>名前</label>
-        <input type="text" name="name" value="{{old('name')}}">
+        <input type="text" name="name" value="{{ Auth::user()->name }}">
         <label>日付</label>
         <input type="text" name="day" value="{{old('day')}}">
         <label>体温</label>
         <input type="text" name="temp" value="{{old('temp')}}">
         <input type="submit" value="投稿">
+        </form>
+        </div>
+    </div>
 </div>
+@endsection
