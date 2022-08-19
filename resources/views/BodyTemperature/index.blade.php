4560747d90cccba00779de8d6e6a3218074d7077
nagasakikoutaro <kmny03@gmail.com>
2022-08-19 14:26:07 +0900

    表示内容を記入する。

--- resources/views/BodyTemperature/index.blade.php
+++ resources/views/BodyTemperature/index.blade.php
@@ -3,11 +3,14 @@
 @section('title','記録一覧')
 
 @section('content')
-
+<div class="row justify-content-center">
+    <div class="col-md-8">
 <h2>記録一覧</h2>
-<p>日付 〇月〇日　体温　〇度</p>
-<p>日付 〇月〇日　体温　〇度</p>
-<p>日付 〇月〇日　体温　〇度</p>
-<p>日付 〇月〇日　体温　〇度</p>
-<p>日付 〇月〇日　体温　〇度</p>
-<p>日付 〇月〇日　体温　〇度</p>
+@foreach ($data as $d )
+    {{$d->name}}
+    {{$d->day}}
+    {{$d->temp}}
+@endforeach
+</div>
+</div>
+@endsection
