ce9043c727e5428fee17a51d022364b4d70a521b
nagasakikoutaro <kmny03@gmail.com>
2022-08-22 08:48:37 +0900

    編集ページへのリンクを作成

--- resources/views/BodyTemperature/index.blade.php
+++ resources/views/BodyTemperature/index.blade.php
@@ -7,9 +7,11 @@
     <div class="col-md-8">
 <h2>記録一覧</h2>
 @foreach ($data as $d )
-    {{$d->name}}
+<p> {{$d->name}}
     {{$d->day}}
     {{$d->temp}}
+    <a href="/edit/{{$d->id}}">編集</a>
+</p>
 @endforeach
 </div>
 </div>
