4dd4c5b2ef866f5b590c0ffae0b04cf3e19c22c7
nagasakikoutaro <kmny03@gmail.com>
2022-08-22 08:47:07 +0900

    編集画面・機能の追加

--- app/Http/Controllers/BodyTemperatureController.php
+++ app/Http/Controllers/BodyTemperatureController.php
@@ -30,6 +30,30 @@ class BodyTemperatureController extends Controller
         return redirect('/index');
     }
 
+    public function edit(Request $request){
+     $data = Bodytemperature::find($request->id);
+        return view('BodyTemperature.edit',compact('data'));
+    }
+
+    public function update(Request $request){
+      
+      $data = Bodytemperature::find($request->id);
+      $data_form = $request->all();
+
+      unset($data_form['_token']);
+
+      $data->fill($data_form)->save();
+      return redirect('/index');
+    }
+
+    public function delete(Request $request){
+        $data = BodyTemperature::find($request->id);
+
+        $data->delete();
+        return redirect('/index');
+    }
+
+
     public function index(Request $request){
         
          $name=Auth::user()->name;
