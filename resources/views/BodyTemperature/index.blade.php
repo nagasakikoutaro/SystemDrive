364d7d81c0318b50415e1074e235deaffebc32f3
nagasakikoutaro <kmny03@gmail.com>
2022-08-25 10:08:39 +0900

    画面レイアウトの変更

--- resources/views/BodyTemperature/index.blade.php
+++ resources/views/BodyTemperature/index.blade.php
@@ -6,13 +6,25 @@
 <div class="row justify-content-center">
     <div class="col-md-8">
 <h2>記録一覧</h2>
-@foreach ($data as $d )
-<p> {{$d->name}}
-    {{$d->day}}
-    {{$d->temp}}
-    <a href="/edit/{{$d->id}}">編集</a>
-</p>
-@endforeach
+<div class="row">
+    <div class="list-news col-md-12 mx-auto">
+        <div class="row">
+            @foreach ($data as $d )
+            <table class="table"  bgcolor="#a9a9a9">
+                <thead>
+                    <tr>
+                       <th width = "20%"> {{$d->name}}</th>
+                       <th width = "20%"> {{$d->day}}</th>
+                       <th width = "20%"> {{$d->temp}}度</th>
+                       <th width = "20%"><a href="/edit/{{$d->id}}">編集</a></th>
+                    </tr>
+                </thead>
+                </tbody>
+            </table>
+            @endforeach
+        </div>
+    </div>
+</div>
 </div>
 </div>
 @endsection
